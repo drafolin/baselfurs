8c1aa57867ab6d0f846d71b38dc716a816096150
Dråfølin <derg@drafolin.ch>
2024-04-05 00:13:09 +0200

    finished footer

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,15 +1,11 @@
-<script setup lang="ts">
-import Icon from "@/assets/icon-192x192.png"
+<script lang="ts" setup>
+import Icon from '@/assets/icon-192x192.png'
 </script>
 
 <template>
   <header>
     <RouterLink to="/">
-      <img
-          class="icon"
-          :src="Icon"
-          alt="Basel Furs"
-      />
+      <img :src="Icon" alt="Basel Furs" class="icon" />
     </RouterLink>
     <h1>Basel Furs</h1>
     <nav>
@@ -23,41 +19,43 @@ import Icon from "@/assets/icon-192x192.png"
   </header>
 </template>
 
-<style scoped lang="scss">
-  header {
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
-    justify-items: center;
-    align-items: center;
-    padding: 0 1.75em;
-    box-shadow: black 0 0 10px;
-    position: fixed;
-    width: 100vw;
-    height: 7rem;
+<style lang="scss" scoped>
+header {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  justify-items: center;
+  align-items: center;
+  padding: 0 1.75em;
+  box-shadow: black 0 0 10px;
+  position: fixed;
+  width: 100vw;
+  height: 7rem;
+  background-color: var(--background);
 
-    :first-child {
-      justify-self: start;
-    }
+  :first-child {
+    justify-self: start;
+  }
 
-    :last-child {
-      justify-self: end;
-    }
+  :last-child {
+    justify-self: end;
+  }
 
-    .icon {
-      height: 4em;
-    }
+  .icon {
+    height: 4em;
+  }
 
-    nav {
-      ul {
-        list-style: none;
-        display: flex;
-        flex-direction: row;
-        li {
-          a {
-            font-size: 1.5em;
-          }
+  nav {
+    ul {
+      list-style: none;
+      display: flex;
+      flex-direction: row;
+
+      li {
+        a {
+          font-size: 1.5em;
         }
       }
     }
   }
+}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,6 @@
 
 <template>
 <main>
-  
 </main>
 </template>
 
