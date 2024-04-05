89944ef589a76dd1df8d8220376e96a597a40b9d
Dråfølin <derg@drafolin.ch>
2024-04-05 16:31:39 +0200

    first section of main page finished

---
+++ src/components/EventList.vue
@@ -0,0 +1,115 @@
+<script lang="ts" setup>
+import type { Event } from '@/models/events'
+import Star from '@/assets/icons/star.png'
+
+const props = defineProps<{
+  events: Event[]
+}>()
+</script>
+
+<template>
+  <ul>
+    <template v-for="event in props.events" :key="event.dateFrom.valueOf() + event.name">
+      <li>
+        <div :class="event.featured ? 'featured' : ''">
+          <div class="date">
+            <span class="month">
+              {{ event.dateFrom.toLocaleString('default', { month: 'short' }) }}
+            </span>
+            <span class="date">
+              {{ event.dateFrom.getDate() }}
+            </span>
+          </div>
+          <div class="details">
+            <div>
+              <img v-if="event.featured" :src="Star" alt="Featured" aria-label="Featured" />
+              <span>
+                {{
+                  event.dateFrom.getHours().toString().padStart(2, '0') +
+                  ':' +
+                  event.dateFrom.getMinutes().toString().padStart(2, '0')
+                }}
+              </span>
+              <span v-if="event.dateTo">
+                {{
+                  '&nbsp;- ' +
+                  event.dateTo.getHours().toString().padStart(2, '0') +
+                  ':' +
+                  event.dateTo.getHours().toString().padStart(2, '0')
+                }}
+              </span>
+            </div>
+            <div>
+              {{ event.name }}
+            </div>
+          </div>
+        </div>
+      </li>
+      <hr />
+    </template>
+  </ul>
+</template>
+
+<style lang="scss" scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+
+  li {
+    margin: 0.5em 0;
+    padding: 0;
+
+    > div {
+      display: flex;
+      flex-direction: row;
+      align-items: stretch;
+      padding: 0.25em 1.5em;
+
+      &.featured {
+        border-left: 2px solid $accent;
+      }
+
+      > .date {
+        display: flex;
+        flex-direction: column;
+        margin-right: 1.25em;
+        justify-content: space-evenly;
+
+        > .date {
+          font-size: 2rem;
+          font-weight: normal;
+        }
+      }
+
+      .details {
+        display: flex;
+        flex-direction: column;
+        align-items: start;
+        justify-content: space-evenly;
+
+        > div:first-child {
+          display: flex;
+          flex-direction: row;
+          align-items: center;
+          justify-content: center;
+
+          img {
+            width: 1em;
+            margin: 0 0.25em;
+
+            @media screen and (prefers-color-scheme: dark) {
+              filter: invert(100%);
+            }
+          }
+        }
+      }
+    }
+  }
+
+  hr {
+    width: 60%;
+    margin: 0.5em auto;
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,86 @@
-<script setup lang="ts">
+<script lang="ts" setup>
+import MeetPic from '@/assets/meetpic.jpg'
+import EventList from '@/components/EventList.vue'
+import { fetchNext } from '@/models/events'
 
+const events = fetchNext(3)
 </script>
 
 <template>
-<main>
-</main>
+  <main>
+    <section class="welcome">
+      <div class="sub">
+        <div class="sub">
+          <h1>Welcome to the Basel furries!</h1>
+          <p>We are a fuzzy community in the Basel area</p>
+        </div>
+        <div class="sub calendar-container">
+          <h2>Calendar - Events</h2>
+
+          <EventList :events="events" class="calendar" />
+
+          <button class="primary">Open Calendar</button>
+        </div>
+      </div>
+
+      <div class="pic">
+        <img
+          :src="MeetPic"
+          alt="A picture of the furmeet, with two fursuited furs in the foreground"
+          class="meet-pic"
+        />
+      </div>
+    </section>
+
+    <section class="learn-more"></section>
+
+    <section class="organizers"></section>
+  </main>
 </template>
 
-<style scoped lang="scss">
+<style lang="scss" scoped>
+main {
+  > section {
+    display: flex;
+    flex-direction: row;
+    min-height: calc(100vh - 7rem);
+    align-items: stretch;
 
+    div.sub {
+      display: flex;
+      flex-direction: column;
+      align-items: stretch;
+      justify-content: center;
+      text-align: center;
+      padding: 1em;
+    }
+
+    > div.sub {
+      padding: 1em 6.25em;
+    }
+
+    .calendar-container {
+      .calendar {
+        width: 80%;
+        margin: auto;
+      }
+
+      button {
+        width: fit-content;
+        margin: 3em auto 0;
+      }
+    }
+  }
+}
+
+div.pic {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.meet-pic {
+  width: 30em;
+}
 </style>
