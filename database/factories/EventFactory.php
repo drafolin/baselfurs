46c75c3345cc4d81682f780f3b93d172af8b2157
drafolin <derg@drafolin.ch>
2024-05-16 14:54:38 +0200

    added seeder

---
+++ database/factories/EventFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Event;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Event>
+ */
+class EventFactory extends Factory
+{
+    /**
+     * Le nom du modèle correspondant.
+     *
+     * @var string
+     */
+    protected $model = Event::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $end_date = fake()->dateTimeThisMonth();
+        return [
+            'name' => fake()->words(3, true),
+            'identifier' => fake()->unique()->uuid(),
+            'start_str' => fake()->dateTimeThisMonth($end_date),
+            'end_str' => $end_date,
+        ];
+    }
+}
